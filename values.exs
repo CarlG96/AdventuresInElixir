a381e6a8528419c5a58fc2f3b0f150e4163d11ce
Carl Gardiner <carl.gardiner@os.uk>
2023-07-02 09:15:18 +0100

    initial commit

---
+++ values.exs
@@ -0,0 +1,14 @@
+# integer
+IO.puts(10)
+
+# string
+IO.puts("I don't like math!")
+
+# Adding integers
+IO.puts(1 + 1)
+
+# Adding floats
+IO.puts(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 )
+
+# Will generate an error
+# IO.puts("Hello World" + 5)
