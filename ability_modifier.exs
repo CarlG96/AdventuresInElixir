495dc42cbf760e19ff19bb57638c5d7c6428106d
Carl Gardiner <carl.gardiner@os.uk>
2023-07-09 08:01:07 +0100

    added some files

--- ability_modifier.exs
+++ ability_modifier.exs
@@ -1,5 +1,12 @@
 ## Pattern matching for D & D
 user_input = IO.gets("Write your ability score:\n")
-{ability_score, _} = Integer.parse(user_input) ## Wildcard character puts the remaining text that wasn't parsed into an integer into it
-ability_modifier = (ability_score - 10) / 2 ## so 1 returns the same as 1 and 2 as only the first integer is parsed
-IO.puts "Your ability modifier is #{ability_modifier}"
+result = case Integer.parse(user_input) do
+  :error -> "Invalid ability score"
+  {ability_score, _} when ability_score >= 0 ->
+  ability_modifier = (ability_score - 10) / 2
+  "Your ability modifier is #{ability_modifier}"
+  {ability_score, _} ->
+  "Number must be positive!"
+end
+
+IO.puts result
